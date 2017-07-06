915a5adb37def6845836a22cc82b7f15ccf2160a
Martin Geisler <martin@geisler.net>
2017-07-06 14:25:57 +0200

    rust: clearly separate Rust from Python

--- talks/2017-04-06-rust/05-python-module/src/lib.rs
+++ talks/2017-04-06-rust/05-python-module/src/lib.rs
@@ -11,16 +11,19 @@ py_module_initializer!(libpython_module,
     try!(m.add(py, "__doc__", "This module is implemented in Rust."));
     try!(m.add(py,
                "word_count",
-               py_fn!(py, word_count(text: PyString))));
+               py_fn!(py, word_count_py(text: PyString))));
     Ok(())
 });
 
-fn word_count(py: Python,
-              text: PyString)
-              -> PyResult<HashMap<String, u64>> {
+fn word_count_py(py: Python,
+                 text: PyString)
+                 -> PyResult<HashMap<String, u64>> {
     let s = text.to_string(py)?;
-    let mut counts = HashMap::new();
+    Ok(word_count(s.into_owned()))
+}
 
+pub fn word_count(s: String) -> HashMap<String, u64> {
+    let mut counts = HashMap::new();
     for word in s.split_whitespace() {
         if word.len() < 5 {
             continue;
@@ -28,5 +31,5 @@ fn word_count(py: Python,
         let lower = word.to_lowercase();
         *counts.entry(String::from(lower)).or_insert(0) += 1;
     }
-    Ok(counts)
+    counts
 }
